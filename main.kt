d92d9050d0d4123b166542bf1d5c45a0bff20617
weilintw <85976295+weilintw@users.noreply.github.com>
2021-06-16 16:57:08 +0800

    Update main.kt
    V4:判斷名字長度

--- main.kt
+++ main.kt
@@ -1,7 +1,15 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("你好，世界！ ！")
-    println("請問大名？ ？")
+fun getStringLength(obj: Any): Int? {
+    if (obj is String) {
+        return obj.length
+    }
+    return null
+}
+fun main() {
+    fun printLength(obj: Any) {
+        println("'$obj' 您的名字共有 ${getStringLength(obj) ?: "... 錯誤 , 這不是一個字串 "} 個字 ")
+    }
+    println("請問大名？")
     val name = readLine()
     println("您好， $name！！")
+    printLength("$name")
 }
